00b623f9aed6e2b94805f35f74297338e0c5e27e
Henit3 <henit33@gmail.com>
2023-05-15 13:19:57 +0100

    Solved the first 3 exercises

--- theories/intro.v
+++ theories/intro.v
@@ -1,7 +1,7 @@
 From Wasm Require Export common.
 From mathcomp Require Import ssreflect ssrfun ssrnat ssrbool eqtype seq.
 From Coq Require Import Program.Equality NArith ZArith_base.
-From Wasm Require Export typing opsem properties interpreter_func.
+From Wasm Require Export typing opsem properties interpreter_func type_progress.
 
 Section intro.
 
@@ -34,8 +34,7 @@ Section intro.
    *)
   Let reduce_trans := @reduce_trans host_function host_instance.
   
-  Let config_tuple := config_tuple host_instance.
-
+(*Let config_tuple := config_tuple host_instance.*)
 
   (* For all (s of type T), Q, P:
    * Hypotheses:
@@ -48,91 +47,21 @@ Section intro.
   Lemma test: forall {T: Type} (s: T) Q P ,
       ((exists s, Q s) -> P) -> (Q s) -> P.
   Proof.
-    (* Simpler:
-    intros T s Q P H0 H1.
-    apply H0. exists s. apply H1.
-    *)
     move => T s Q P Himpl HQ.
     apply Himpl; eexists; by apply HQ.
   Qed.
-
-  (* Simpler extra practice lemmas *)
-
-  Lemma tuple_to_trans: forall hs1 s1 f1 es1 hs2 s2 f2 es2,
-    reduce_trans (hs1, s1, f1, es1) (hs2, s2, f2, es2) ->
-    reduce_tuple (hs1, s1, f1, es1) (hs2, s2, f2, es2).
-  Proof.
-    intros.
-  Admitted.
-  Lemma reduce_to_tuple: forall hs1 s1 f1 es1 hs2 s2 f2 es2,
-    reduce_tuple (hs1, s1, f1, es1) (hs2, s2, f2, es2) ->
-    reduce hs1 s1 f1 es1 hs2 s2 f2 es2.
-  Proof.
-    intros.
-    unfold reduce_tuple in H.
-    apply H.
-  Qed.
-
-  Lemma reduce_to_trans: forall hs1 s1 f1 es1 hs2 s2 f2 es2,
-    reduce_trans (hs1, s1, f1, es1) (hs2, s2, f2, es2) ->
-    reduce hs1 s1 f1 es1 hs2 s2 f2 es2.
-  Proof.
-    intros.
-    apply reduce_to_tuple.
-    apply tuple_to_trans.
-    apply H.
-  Qed.
-  Lemma reduce_from_trans: forall hs1 s1 f1 es1 hs2 s2 f2 es2,
-    reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
-    reduce_trans (hs1, s1, f1, es1) (hs2, s2, f2, es2).
-  Proof.
-    intros. unfold reduce_trans.
-    apply Relations.Relation_Operators.rt_step.
-    unfold reduce_tuple. apply H.
-  Qed.
   
-  Lemma opsem_reduce_seq0: forall hs1 s1 f1 es1 hs2 s2 f2 es2 hs3 s3 f3 es3,
-      reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
-      reduce hs2 s2 f2 es2 hs3 s3 f3 es3 ->
-      reduce hs1 s1 f1 es1 hs3 s3 f3 es3.
-  Proof.
-    intros. rename H into H12. rename H0 into H23.
-    cut (reduce_tuple (hs1, s1, f1, es1) (hs2, s2, f2, es2)). 2: { apply H12. } intros H12'.
-    cut (reduce_tuple (hs2, s2, f2, es2) (hs3, s3, f3, es3)). 2: { apply H23. } intros H23'.
-    cut (reduce_trans (hs1, s1, f1, es1) (hs3, s3, f3, es3)). 2 : {
-      apply Relations.Relation_Operators.rt_trans with (hs2, s2, f2, es2).
-      - apply Relations.Relation_Operators.rt_step. apply H12.
-      - apply Relations.Relation_Operators.rt_step. apply H23.
-    } intros H13''.
-    apply reduce_to_trans. apply H13''.
-  Qed.
+      
 
-  Lemma trans_result: forall hs1 s1 f1 es1 hs2 s2 f2 es2,
-    reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
-    exists hs3 s3 f3, reduce hs3 s3 f3 [::] hs3 s3 f3 [::] ->
-    reduce hs1 s1 f1 es1 hs3 s3 f3 [::].
-  Proof.
-    intros. (* (Exists stops this proof)
-    apply reduce_to_trans.
-    apply Relations.Relation_Operators.rt_refl.*)
-  Admitted.
-  Lemma trans_split: forall hs1 s1 f1 es1 hs2 s2 f2 es2 es3,
-    reduce_trans (hs1, s1, f1, (es1 ++ es3)) (hs2, s2, f2, (es2 ++ es3)) ->
-    reduce_trans (hs1, s1, f1, es1) (hs2, s2, f2, es2) ->
-    (exists hs3 s3 f3,
-      reduce_trans (hs2, s2, f2, es2) (hs3, s3, f3, [::]) ->
-      reduce_trans (hs2, s2, f2, (es2 ++ es3)) (hs3, s3, f3, es3)) ->
-    reduce_trans (hs1, s1, f1, (es1 ++ es3)) (hs2, s2, f2, (es2 ++ es3)).
-  Proof. (* Test lemma *) Admitted.
-
-
+  
   (* Operational Semantics: Reduction with equivalent base instructions + identical instruction
    * If the base instructions reduce to the same result (i.e. are equivalent),
    *  Appending identical instructions to both sides will still give the same result.
    *)
 
-Search (_ ++ nil). (* cats0 *)
-Search AI_trap.
+  Locate "/".
+  Search (_ ++ nil). (* cats0 *)
+  Search AI_trap.
 
   (* Reduction of a sequence of commands.
       There's no `explicit` rule that allows this to be possible
@@ -143,111 +72,77 @@ Search AI_trap.
       reduce hs1 s1 f1 (es1 ++ es0) hs2 s2 f2 (es2 ++ es0).
   Proof.
     intros.
-
-    (* Perhaps try to obtain reduce (x, x0, x1, es0) (x, x0, x1, es0)?
-    apply reduce_to_trans. pose proof H as H0. pose proof H as Ht.
-    apply trans_result in H0. destruct H0. destruct H0. destruct H0.
-    apply reduce_from_trans in Ht.
+    (* (e)Apply r_label yields same proof structure as reduce_composition_right *)
+  (*eapply r_label; eauto; apply /lfilledP;
+    [instantiate (1 := (LH_base [::] es0)); instantiate (1 := 0)|];
+    [replace es1 with ([::] ++ es1) at 2|replace es2 with ([::] ++ es2) at 2];
+    reflexivity; apply LfilledBase; reflexivity.*)
+  (* or *)
+  (*eapply r_label; eauto; apply /lfilledP.
+    - instantiate (1 := (LH_base [::] es0)). instantiate (1 := 0).
+      replace es1 with ([::] ++ es1) at 2; try (reflexivity).
+      apply LfilledBase. reflexivity.
+    - replace es2 with ([::] ++ es2) at 2; try (reflexivity).
+      apply LfilledBase. reflexivity.*)
     
-    cut (reduce_trans (x, x0, x1, [::]) (x, x0, x1, [::])). 2: {
-      apply Relations.Relation_Operators.rt_refl.
-    } intros Ht_33.
-    apply reduce_to_trans in Ht_33. apply H0 in Ht_33 as Ht_13.
-    apply reduce_from_trans in Ht_33. apply reduce_from_trans in Ht_13.
-    (*cut (reduce_trans (hs2, s2, f2, es2) (x, x0, x1, [::])). 2: {
-      apply Relations.Relation_Operators.rt_trans with (hs1, s1, f1, es1).
-      { admit. (* eq, should be symmetric? *) } { apply Ht_13. }
-    } intros Ht_23.*)
+    (* From type_progress.v, makes use of general reduce_composition *)
+    apply reduce_composition_right; assumption.
+  (*apply H. *)
+  Qed.
 
-    apply trans_split. 2: { apply Ht. } 2: {
-      exists x. exists x0. exists x1. intros Ht_23'.
-      admit.
-    }*)
-
-  (* False, since no guarantee instructions give same output for all inputs
-    cut (reduce_trans (hs1, s1, f1, es0) (hs2, s2, f2, es0)). 2: {
-      admit. 
-    } intros Ht_es0. *)
-    (* (1,1+0) -> (2,2+0) same as (1,1+0) -> (1',1) -> (2',2) -> (2,2+0) *)
-    (* Relation not symmetric so cannot do this; should it be? *)
-
-  (*induction es0 as [|e es' iHyp].
-    - rewrite cats0. rewrite cats0. apply H.*)
-      (* STUCK:
-       *  Case adds instruction between es# and es' instead of at end.
-       *    Could fix with custom induction?
-       *  Also don't know how to simplify reduction regardless.
-       *    Apply Relations.Relation_Operators.rt_trans
-       *      with (y := [iHyp])
-       *    Or Relations.Relation_Operators.rt_step? [Didn't work]
-       *)
-  (*- inversion iHyp. {} destruct e eqn:eEqn; simpl. *)
-  Admitted.
-
-  (* Helper lemma for opsem_reduce_seq2 (reduce one const) *)
-  Lemma opsem_reduce_seq2': forall hs1 s1 f1 es1 hs2 s2 f2 es2 v,
-    is_const v ->
-    reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
-    reduce hs1 s1 f1 (v :: es1) hs2 s2 f2 (v :: es2).
-  Proof.
-    intros.
-    destruct v eqn:vEqn; try ( destruct b eqn:bEqn );
-    simpl in H; try ( discriminate ).
-    
-    (* REDUCED DOWN TO SINGLE CASE (STUCK):
-        reduce hs1 s1 f1 (AI_basic (BI_const v0) :: es1)
-              hs2 s2 f2 (AI_basic (BI_const v0) :: es2)
-    *)
-    admit.
-    (*inversion H0; try (apply r_simple (*contructor*)). {
-       
-    }*)
-(*? cut (reduce_tuple (hs1, s1, f1, (AI_basic (BI_const v0) :: es1)) (hs2, s2, f2, (AI_basic (BI_const v0) :: es2))). *)
-(*? induction es1 as [|h1 t1 iHyp1]; induction es2 as [|h2 t2 iHyp2]. { } *)
-(*? inversion H0; try (apply r_simple). { } *)
-  Admitted.
-
-  (* Operational Semantics: Reduction with equivalent base instructions + constant value stack
-   * Given a list/stack of constants,
-   *  Prepending them to equivalent instruction will give the same result.
-   *)
-
-  (* Now note that given Wasm's representation of value stack squashed into the instruction list,
-      adding more constants to the bottom of the value stack should not matter as well. *)
+  (* Now note that given Wasm's representation of value stack squashed into the instruction list, adding more constants to the bottom of the value stack should not matter as well. *)
   Lemma opsem_reduce_seq2: forall hs1 s1 f1 es1 hs2 s2 f2 es2 vs,
       const_list vs ->
       reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
       reduce hs1 s1 f1 (vs ++ es1) hs2 s2 f2 (vs ++ es2).
   Proof.
-      intros. rename H into Hvs. rename H0 into Hred.
-      induction vs as [|v vs' iHyp]; simpl.
-      - apply Hred.
-      - apply Bool.andb_true_iff in Hvs.
-        destruct Hvs as [Hv Hvs']. apply iHyp in Hvs'.
-        apply opsem_reduce_seq2'; (apply Hv || apply Hvs').
-  (*! Uses admitted opsem_reduce_seq2' !*)
+    intros.
+    (* (e)Apply r_label yields same proof structure as reduce_composition_left *)
+  (*eapply r_label; eauto; apply /lfilledP;
+    [instantiate (1 := (LH_base vs [::])); instantiate (1 := 0)|];
+    [replace es1 with (es1 ++ [::]) at 2|replace es2 with (es2 ++ [::]) at 2];
+    try (apply cats0); apply LfilledBase; apply H.*)
+  (* or *)
+  (*eapply r_label; eauto; apply /lfilledP.
+    - instantiate (1 := (LH_base vs [::])). instantiate (1 := 0).
+      replace es1 with (es1 ++ [::]) at 2; try (apply cats0).
+      apply LfilledBase. apply H.
+    - replace es2 with (es2 ++ [::]) at 2; try (apply cats0).
+      apply LfilledBase. apply H.*)
+    
+    (* From type_progress.v, makes use of general reduce_composition *)
+    apply reduce_composition_left; assumption.
+  (*- apply H.
+    - apply H0.*)
   Qed.
 
-
-  (* Operational Semantics: Reduction with equivalent base instructions (combination of above)
-   *)
-
-  (* This lemma can of course be derived from the two above.
-      However, its proof itself is even simpler. *)
+  (* This lemma can of course be derived from the two above. However, its proof itself is
+     even simpler. *)
   Lemma opsem_reduce_seq3: forall hs1 s1 f1 es1 hs2 s2 f2 es2 vs es0,
       const_list vs ->
       reduce hs1 s1 f1 es1 hs2 s2 f2 es2 ->
       reduce hs1 s1 f1 (vs ++ es1 ++ es0) hs2 s2 f2 (vs ++ es2 ++ es0).
   Proof.
-(* REVISIT: prove this from scratch. *)
-    intros. rename H into Hvs. rename H0 into Hred.
-    apply opsem_reduce_seq2. apply Hvs.
-    apply opsem_reduce_seq1. apply Hred.
-(*! Uses admitted opsem_reduce_seq1 and opsem_reduce_seq2' !*)
+    intros.
+    (* (e)Apply r_label yields same proof structure as reduce_composition *)
+  (*eapply r_label; eauto; apply /lfilledP;
+    [instantiate (1 := (LH_base vs es0)); instantiate (1 := 0)|];
+    apply LfilledBase; apply H.*)
+    (* or *)
+  (*eapply r_label; eauto; apply /lfilledP.
+    - instantiate (1 := (LH_base vs es0)). instantiate (1 := 0).
+      apply LfilledBase. apply H.
+    - apply LfilledBase. apply H.*)
+
+    (* From type_progress.v, this is the general reduce_composition form *)
+    apply reduce_composition; assumption.
+  (*- apply H.
+    - apply H0.*)
   Qed.
 
   
   
+
 (*** DEPRECATED
   (* Typing lemmas *)
 
@@ -283,6 +178,7 @@ Search AI_trap.
   Proof.
   Admitted.
 ***)
+  
 
 
   
